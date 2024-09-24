5d7ceab350833415ea2ea2d551aa8e2ecf4f23af
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-24 10:37:53 +0800

    feat: add the image select functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,26 @@
 const productImg = document.querySelector("#product-img");
 const btn = document.querySelectorAll(".btn");
 
+btn[0].onclick = function() {
+  productImg.src = "./images/image1.png";
+  for (bt of btn) {
+    bt.classList.remove("active");
+  }
+  this.classList.add("active");
+}
+
+btn[1].onclick = function() {
+  productImg.src = "./images/image2.png";
+  for (bt of btn) {
+    bt.classList.remove("active");
+  }
+  this.classList.add("active");
+}
+
+btn[2].onclick = function() {
+  productImg.src = "./images/image3.png";
+  for (bt of btn) {
+    bt.classList.remove("active");
+  }
+  this.classList.add("active");
+}
